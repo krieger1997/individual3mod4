03b8ab7f6a1430284f9d2c29c6382e2fe0ab3696
krieger <claudio_krieger@hotmail.com>
2023-05-17 19:42:43 -0400

    listoco

--- src/main.js
+++ src/main.js
@@ -1,26 +1,29 @@
 
 
-const URL = "www.themealdb.com/api/json/v1/1/";
+const URL = "https://www.themealdb.com/api/json/v1/1/";
 const endpoint = "categories.php";
 
 const muestraCategorias= async ()=>{
   
-  await fetch(`${URL}${endpoint}`).then(response=>{
+  await fetch(`${URL}${endpoint}`).then( async (response)=>{
     if(!response.ok){
       throw new Error(`HTTP ERROR.\n El status code es ${response.status}\n ${URL}${endpoint}`)
     }
-    response.json().then(data=>{
-      console.log("DATA: ",data)
+    await response.json().then(data=>{
+      console.log("============== INICIO =============")
+      console.log("DATA GENERAL: ",data.categories)
+      data.categories.forEach((element) => {
+        console.log('%c CATEGORÍA: '+element.strCategory, 'background: #222; color: #bada55');
+        console.table(element)
+      });
     })
-
-
-
 
   }).catch(error=>{
     console.error(error)
   })
 
-
 }
-
-muestraCategorias();
+  
+muestraCategorias().then(res=>{
+  console.log("============== FIN =============")
+})
